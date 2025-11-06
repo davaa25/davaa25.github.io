f3d9431132ef1756b6f10849cda0cc893e62502f
davaa25 <156521684+davaa25@users.noreply.github.com>
2025-11-06 22:01:02 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     const response = await fetch(`https://api-inference.huggingface.co/models/${MODEL}`, {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${HF_TOKEN}`,
+        Authorization: `Bearer ${}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
